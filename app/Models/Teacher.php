83b4f6e9966661b5f22dad0fc6b69aa459e51095
serhii_babik <serhii.babik@gmail.com>
2025-02-09 23:17:17 +0000

    Create Teacher entity

---
+++ app/Models/Teacher.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $first_name
+ * @property string $last_name
+ */
+class Teacher extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->first_name . ' ' . $this->last_name,
+        );
+    }
+}
