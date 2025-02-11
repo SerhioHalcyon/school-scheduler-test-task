6966a092990053e4800d46db72e5a590e2c60e53
serhii_babik <serhii.babik@gmail.com>
2025-02-11 21:50:32 +0200

    Create schedule entity

---
+++ app/Models/Schedule.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+/**
+ * @property int $id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
+ *
+ */
+class Schedule extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    public function bell(): BelongsTo
+    {
+        return $this->belongsTo(Bell::class);
+    }
+
+    public function schoolClass(): BelongsTo
+    {
+        return $this->belongsTo(SchoolClass::class);
+    }
+
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             SchoolClassSeeder::class,
             SubjectTeacherSeeder::class,
             ClassSubjectSeeder::class,
+            BellSeeder::class,
         ]);
     }
 }
